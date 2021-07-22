8743654d96a8abf9c537a2fb368169a2498f2e52
Nguyen Ngoc Huyen <45890409+Noragamiiii@users.noreply.github.com>
2021-07-23 00:37:04 +0700

    fix query users

--- src/components/getuser/User.js
+++ src/components/getuser/User.js
@@ -6,6 +6,7 @@ import { getTokens } from "../../auth/AuthToken";
 import { setContext } from "apollo-link-context";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { Button } from "antd";
 import "./User.css";
 
 import { createHttpLink } from "apollo-link-http";
@@ -15,46 +16,52 @@ const token = getTokens();
 
 export default function User() {
   const [user, setUser] = useState([]);
-  const authlink = setContext(async (_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        Authorization: token ? `Bearer ${token}` : "",
-      },
-    };
-  });
-  const client = new ApolloClient({
-    link: authlink.concat(httpLink),
-    cache: new InMemoryCache(),
-    connectToDevTools: true,
-  });
-  client
-    .query({
-      query: gql`
-        query {
-          users {
-            hits {
-              first_name
-              last_name
+  const handleGetUser = () => {
+    const authlink = setContext(async (_, { headers }) => {
+      return {
+        headers: {
+          ...headers,
+          Authorization: token ? `Bearer ${token}` : "",
+        },
+      };
+    });
+    const client = new ApolloClient({
+      link: authlink.concat(httpLink),
+      cache: new InMemoryCache(),
+      connectToDevTools: true,
+    });
+    client
+      .query({
+        query: gql`
+          query {
+            users {
+              hits {
+                first_name
+                last_name
+              }
             }
           }
-        }
-      `,
-    })
-    .then((result) => {
-      setUser(result.data.users.hits);
-      console.log("user", user);
-      // console.log("data", result);
-    })
-    .catch((error) => console.log("error", error));
+        `,
+      })
+      .then((result) => {
+        setUser(result.data.users.hits);
+        console.log("user", user);
+        // console.log("data", result);
+      })
+      .catch((error) => console.log("error", error));
+  };
   return (
     <div className="userContainer">
+      <Button type="primary" onClick={() => handleGetUser()}>
+        Show User
+      </Button>
       <ul>
-        {user.map((u) => (
-          <li>
-            {u.first_name} {u.last_name}
-          </li>
-        ))}
+        {user &&
+          user.map((u) => (
+            <li>
+              {u.first_name} {u.last_name}
+            </li>
+          ))}
       </ul>
     </div>
   );
